50d59df7cf7ed536b37982c358285c9f4ec425e0
Robert Walker <robertswalker@gmail.com>
2023-02-24 22:17:26 -0600

    checking buttons

--- emlvidplayerbuttons.py
+++ emlvidplayerbuttons.py
@@ -65,71 +65,71 @@ while True:
     # If side button is pressed reboot the system
     if GPIO.input(gpio_reboot_pin) == GPIO.HIGH:
         print("Reboot pin has been pressed")
-        os.system("reboot")
+        # os.system("reboot")
 
     # If both the yellow and black are pressed together return to desktop
     if GPIO.input(video_1_pin) == GPIO.HIGH and GPIO.input(video_2_pin) == GPIO.HIGH:
         print("Exit fullscreen button sequnce has been pressed")
-        mplayer.set_fullscreen(False)
+        # mplayer.set_fullscreen(False)
     
     # If yellow button is pressed launch 1st video
     if GPIO.input(video_1_pin) == GPIO.HIGH:
         print("Video Button 1 has been pressed")
-        if current_index == 1:
-            if media_player.is_playing() == 0:
-                if media_player.get_state() == 4:
-                    media_player.play()
-                if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
-                    media_player.play_item_at_index(0)
-            else: 
-                media_player.pause()
-        else:
-            media_player.play_item_at_index(0)
-        current_index = 1
+        # if current_index == 1:
+        #     if media_player.is_playing() == 0:
+        #         if media_player.get_state() == 4:
+        #             media_player.play()
+        #         if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
+        #             media_player.play_item_at_index(0)
+        #     else: 
+        #         media_player.pause()
+        # else:
+        #     media_player.play_item_at_index(0)
+        # current_index = 1
     
     # If top black button is pressed launch 2nd video
     if GPIO.input(video_2_pin) == GPIO.HIGH:
         print("Video Button 2 has been pressed")
-        if current_index == 2:
-            if media_player.is_playing() == 0:
-                if media_player.get_state() == 4:
-                    media_player.play()
-                if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
-                    media_player.play_item_at_index(1)
-            else: 
-                media_player.pause()
-        else:
-            media_player.play_item_at_index(1)
-        current_index = 2
+        # if current_index == 2:
+        #     if media_player.is_playing() == 0:
+        #         if media_player.get_state() == 4:
+        #             media_player.play()
+        #         if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
+        #             media_player.play_item_at_index(1)
+        #     else: 
+        #         media_player.pause()
+        # else:
+        #     media_player.play_item_at_index(1)
+        # current_index = 2
     
     #If white button is pressed launch 3rd video
     if GPIO.input(video_3_pin) == GPIO.HIGH:
         print("Video 3 has been pressed")
-        if current_index == 3:
-            if media_player.is_playing() == 0:
-                if media_player.get_state() == 4:
-                    media_player.play()
-                if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
-                    media_player.play_item_at_index(2)
-            else: 
-                media_player.pause()
-        else:
-            media_player.play_item_at_index(2)
-        current_index = 3
+        # if current_index == 3:
+        #     if media_player.is_playing() == 0:
+        #         if media_player.get_state() == 4:
+        #             media_player.play()
+        #         if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
+        #             media_player.play_item_at_index(2)
+        #     else: 
+        #         media_player.pause()
+        # else:
+        #     media_player.play_item_at_index(2)
+        # current_index = 3
     
     # If blue button is pressed launch 4th video
     if GPIO.input(video_4_pin) == GPIO.HIGH:
         print("Video 4 has been pressed")
-        if current_index == 4:
-            if media_player.is_playing() == 0:
-                if media_player.get_state() == 4:
-                    media_player.play()
-                if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
-                    media_player.play_item_at_index(3)
-            else: 
-                media_player.pause()
-        else:
-            media_player.play_item_at_index(3)
-        current_index = 4
+        # if current_index == 4:
+        #     if media_player.is_playing() == 0:
+        #         if media_player.get_state() == 4:
+        #             media_player.play()
+        #         if media_player.get_state() == 5 or media_player.get_state() == 6 or media_player.get_state() == 7:
+        #             media_player.play_item_at_index(3)
+        #     else: 
+        #         media_player.pause()
+        # else:
+        #     media_player.play_item_at_index(3)
+        # current_index = 4
     sleep(0.15)
 print("Program is complete...")
